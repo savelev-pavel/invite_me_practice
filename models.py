2ddb28a1447c50ab9df724e8f9f1cf11edae62e7
pavel.savelev <pal.igory4@gmail.com>
2023-10-30 11:15:43 +0300

    2

--- models.py
+++ models.py
@@ -3,10 +3,17 @@ from pydantic import BaseModel, Field, EmailStr
 
 class User(BaseModel):
     id: int = Field(ge=0, frozen=True)
+    # целое число, больше 0, без возможности изменения
     name: str = Field(default='Безымянный', min_length=1, max_length=50)
+    # текстовый тип, от 1 до 50 символов, по умолчанию значение: Безымянный
     phone: str = Field(default='', min_length=10, max_length=10, pattern=r'^\d*$')
+    # текстовый тип, разрешены только цифры, 10 символов, по умолчанию пустая строка
     email: EmailStr = Field(default='', min_length=3, max_length=50)
+    # тип данных строка е-мэйла, от 3 до 50 символов, по умолчанию пустая строка
 
 
-user = User(id=1, name='П')
-print(user.model_dump())
+try:
+    user = User(id=-1)
+    print(user.model_dump())
+except ValueError:
+    print
