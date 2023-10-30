77b015bfcdf2ee52d647358aa7f48796555eb797
pavel.savelev <pal.igory4@gmail.com>
2023-10-30 11:04:18 +0300

    1

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr
 
 
 class User(BaseModel):
-    id: int
-    name: str = unnamed
-    phone: str
-    mail:
+    id: int = Field(ge=0, frozen=True)
+    name: str = Field(default='Безымянный', min_length=1, max_length=50)
+    phone: str = Field(default='', min_length=10, max_length=10, pattern=r'^\d*$')
+    email: EmailStr = Field(default='', min_length=3, max_length=50)
 
 
+user = User(id=1, name='П')
+print(user.model_dump())
